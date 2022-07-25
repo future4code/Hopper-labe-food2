b78266de49369e05fb19353df3323ca5b39a9a54
unknown <gabrielcandido10@hotmail.com>
2022-07-25 14:45:08 -0300

    FeedPage, 95% concluído

--- labefood/src/components/FeedCards/FeedCards.js
+++ labefood/src/components/FeedCards/FeedCards.js
@@ -1,29 +1,34 @@
 import useGetData from "../../hooks/useGetData";
-import { CardRestaurant } from "./Styles"
+import { CardRestaurant, Title } from "./Styles"
 import { goToDetails } from "../../router/coordinator";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../Spinner/Spinner";
 
-const FeedCards = () => {
+const FeedCards = ({text, filter}) => {
 
   const navigate = useNavigate()
   const { data, isLoading } = useGetData('/restaurants')
-
+  const search = text
+  const category = filter
+  
+  console.log(category)
   return (
     <>
       {isLoading 
       ? (<Spinner />) 
-      : data && data.map(restaurant => (
+      : data?.restaurants?.filter(restaurant => {
+        return restaurant.name.toLowerCase().includes(search.toLowerCase()) && restaurant.category.includes(category)
+      }).map(restaurant => (
         <CardRestaurant
-          key={restaurant.id}
-          onClick={() => goToDetails(navigate, restaurant.id)} 
-        >
+            key={restaurant.id}
+            onClick={() => goToDetails(navigate, restaurant.id)} 
+          >
           <img 
             src={restaurant.logoUrl} 
             alt={`Logo do restaurante ${restaurant.name}`}
           />
           <div>
-            <p>{restaurant.name}</p>
+            <Title>{restaurant.name}</Title>
             <span>
               <p>
                 {`${restaurant.deliveryTime - 10}
@@ -33,7 +38,7 @@ const FeedCards = () => {
             </span>
           </div>
         </CardRestaurant>
-      ))} 
+      ))}
     </>
   )
 }
--- labefood/src/pages/FeedPage/Styles.js
+++ labefood/src/pages/FeedPage/Styles.js
@@ -6,8 +6,9 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   margin-bottom: 10vh;
+  overflow: hidden;
 `;
 
 export const Search = styled.div`
@@ -17,7 +18,6 @@ export const Search = styled.div`
   border: 1px solid #B8B8B8;
   border-radius: 2px;
   display: flex;
-  justify-content: space-between;
   button {
     border: none;
     height: 100%;
@@ -29,12 +29,32 @@ export const Search = styled.div`
     }
   }
   input {
-    width: 80%;
+    width: 100%;
     height: 100%;
     border: none;
     background: transparent;
+    text-indent: 10px;
     &:focus {
       outline: none; 
     }
   }
+`;
+
+export const Categories = styled.div`
+  width: 80%;
+  height: 6vh;
+  display: flex;
+  align-items: center;
+  gap: 1em;
+  white-space: nowrap;
+  overflow: scroll;
+  margin-left: 10%;
+  &::-webkit-scrollbar {
+    width: .1px;
+  }
+  p {
+    width: 100%;
+    text-align: center;
+  }
+  ${({idx}) => (`p:nth-child(${idx}){color: #5cb646}`)}
 `;
